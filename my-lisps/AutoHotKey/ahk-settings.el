f909f1ab151786359853ba04f3d5f1f3bae7cf16
Chunguang <zhengchunguang@baidu.com>
2011-01-10 13:19:43 +0800

    bug fixed in ahk-settings

--- my-lisps/AutoHotKey/ahk-settings.el
+++ my-lisps/AutoHotKey/ahk-settings.el
@@ -1,8 +1,7 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2011-01-10 13:10:08 Monday by ssl>
 (setq ahk-syntax-directory (concat my-emacs-lisps-path "AutoHotKey/Syntax"))
 (add-to-list 'auto-mode-alist '("\\.ahk$" . ahk-mode))
 (autoload 'ahk-mode "ahk-mode")
 
-(provide ahk-settings)
+(provide 'ahk-settings)

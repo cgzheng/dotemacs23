82cb91cbd2c3aa561ce2acc957f53973735b2ac0
Chunguang <zhengchunguang@baidu.com>
2011-01-16 22:38:31 +0800

    set default font size to 15, for use of large monitor

--- my-lisps/font-settings.el
+++ my-lisps/font-settings.el
@@ -1,12 +1,12 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2010-11-22 01:03:13 Monday by work>
+;; Time-stamp: <2011-01-16 22:37:51 Sunday by ssl>
 
 (when (and window-system is-after-emacs-23)
   (require 'my-fontset-win)
   (if mswin
       ;; (huangq-fontset-courier 17)
-      (huangq-fontset-consolas 13)
+      (huangq-fontset-consolas 15)
     ;; (huangq-fontset-dejavu 17)))
     ;; (huangq-fontset-fixedsys 17)
     (set-default-font "Monospace 11")))
